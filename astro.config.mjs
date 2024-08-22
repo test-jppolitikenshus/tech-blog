20eca326dd237f723177d1ce49383b48a5074d2d
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-08-22 16:21:08 +0200

    Update site URL to use HTTPS in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'test-jppolitikenshus.github.io',
+  site: 'https://test-jppolitikenshus.github.io',
   base: '/tech-blog',
   integrations: [mdx(), sitemap()],
 });
