e2095eeaa25400203973f7fb7cdd81c3b5f93611
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-08-22 16:15:26 +0200

    Add deploy workflow for GitHub Pages, update site URL and set base path in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'test-jppolitikenshus.github.io',
+  base: '/tech-blog',
+  integrations: [mdx(), sitemap()],
 });
